b6b8728ba229fb87773aa4f6062e33217217ed60
Ulug2 <khaiul01@gettysburg.edu>
2025-09-24 14:16:29 -0400

    Finished step 6

--- client/public/scripts/header.js
+++ client/public/scripts/header.js
@@ -4,3 +4,30 @@ headerContainer.className = 'header-container'
 
 const headerLeft = document.createElement('div')
 headerLeft.className = 'header-left'
+
+const headerLogo = document.createElement('img')
+headerLogo.src = '/logo.png'
+
+const headerTitle = document.createElement('h1')
+headerTitle.textContent = "UnEarthed"
+
+headerLeft.appendChild(headerLogo)
+headerLeft.appendChild(headerTitle)
+
+
+const headerRight = document.createElement('div')
+headerRight.className = 'header-right'
+
+const headerButton = document.createElement('button')
+headerButton.textContent = "Home"
+
+headerButton.addEventListener('click', function handleClick(event) {
+  window.location = '/'
+})
+
+headerRight.appendChild(headerButton)
+
+headerContainer.appendChild(headerLeft)
+headerContainer.appendChild(headerRight)
+
+header.appendChild(headerContainer)
