f33238ce6f21198085a0d99cb62e76d956b377ff
Ulug2 <khaiul01@gettysburg.edu>
2025-10-01 20:37:47 -0400

    Lab 3 completed

--- client/src/main.js
+++
@@ -1,7 +0,0 @@
-// Main entry point for the Unearthed application
-import '../style.css'
-
-// Initialize the application
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Unearthed app initialized');
-});
